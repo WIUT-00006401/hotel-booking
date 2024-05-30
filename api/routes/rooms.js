ef1eb889a4700752f6fec0eb614afd80491d8777
WIUT_00006401 <durdona.jurayeva@mail.ru>
2024-05-30 23:34:58 +0200

    rooms route

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,22 @@
 import express from "express"
+import { verifyAdmin } from "../utils/verifyToken.js"
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js"
 
 const router = express.Router()
+
+// Create
+router.post("/:hotelid", verifyAdmin, createRoom)
+
+// Update
+router.put("/:id", verifyAdmin, updateRoom)
+
+// Delete
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom)
+
+// Get
+router.get("/:id", getRoom)
+
+// Get All
+router.get("/", getRooms)
 
 export default router
